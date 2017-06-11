e6bb6e481bbb0fa5b33432775bee71b584adcf85
GirishRajasekar <girishgowda.r@gmail.com>
2017-06-11 22:34:47 +0100

    With Latest Change

---
+++ src/test/java/com/jpmorgan/assignment/supersimple/stock/impl/SampleTestCase.java
@@ -0,0 +1,30 @@
+package com.jpmorgan.assignment.supersimple.stock.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+
+public class SampleTestCase {
+	
+	@Before
+	public void setUp() throws Exception{
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception{
+		
+	}
+	
+	@Test
+	public void testForValidCase(){
+		
+	}
+	
+	@Test(expected=Exception.class)
+	public void testForException(){
+		
+	}
+
+}
